2f569be41020fce326d557670e7c4444330112e4
ellacharmed <ellacharmed@gmail.com>
2020-02-02 22:57:35 +0800

    change color value
    change color value from "#FB2707" to "orangered2"

--- Exploratory-Data-Analysis-Week-1-Project/plot1.R
+++ Exploratory-Data-Analysis-Week-1-Project/plot1.R
@@ -3,16 +3,26 @@ library(chron)
 
 setwd("Z:\\odrive\\OneDrive-WatiSahnan\\Repos\\datasciencecoursera\\Exploratory-Data-Analysis-Week-1-Project")
 
-colVariables <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+colVariables <- c("Date"
+                  ,"Time"
+                  ,"Global_active_power"
+                  ,"Global_reactive_power"
+                  ,"Voltage"
+                  ,"Global_intensity"
+                  ,"Sub_metering_1"
+                  ,"Sub_metering_2"
+                  ,"Sub_metering_3")
 mydata <- read.table("household_power_consumption.txt", 
                      sep = ";", 
                      na.strings = "?", 
                      skip = 66637, 
-                     nrows = 2881, 
-                     col.names = colVariables,
-          )
+                     nrows = 2880, 
+                     col.names = colVariables)
 mydata$Date <- as.Date(mydata$Date, "%d/%m/%Y")
-mydata$Time <- test <- times(mydata$Time)
+
 png(filename = "plot1.png", width = 480, height = 480, units = "px")
-with(mydata, hist(Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="#FB2707"))
+with(mydata, hist(Global_active_power
+           , main="Global Active Power"
+           , xlab = "Global Active Power (kilowatts)"
+           , col="orangered2"))
 dev.off()
